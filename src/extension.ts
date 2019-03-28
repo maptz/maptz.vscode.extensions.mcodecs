74ddbc5363bf9e3934b811d97d6ea8b2f78f73ea
Maptz <info@maptz.com>
2019-03-29 10:44:20 +1100

    Some change

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,16 @@ import * as tools from "./tool";
 import * as config from "./configuration";
 import * as codechange from "./codechange";
 
-const debugMode = false;
-console.log('Congratulations, your extension "mcodecs" is now active!');
+const useDebugMode = true;
+const useStreamMode = false;
+
 const outputChannelName = "Maptz.MCodeCS";
 let outputChannel = vscode.window.createOutputChannel(outputChannelName);
 
-const toolInitializer = new tools.ToolInitializer(config.Configuration.loadConfiguration(debugMode), outputChannel);
+const toolInitializer = new tools.ToolInitializer(
+  config.Configuration.loadConfiguration(useDebugMode),
+  outputChannel
+);
 toolInitializer.initialize();
 
 export class ToolRunner {
@@ -30,25 +34,29 @@ export class ToolRunner {
 
   private runInternal(commandName: string) {
     //outputChannel.show();
-    
+
     const textEditor = vscode.window.activeTextEditor;
     if (!textEditor) {
       outputChannel.appendLine(`No editor. Exiting.`);
       return;
     }
-    if (toolInitializer.hasError){
+    if (toolInitializer.hasError) {
       const issue = `Maptz.MCodeCS. Error running command '${commandName}'. The tool has not installed.`;
       outputChannel.appendLine(issue);
-      vscode.window.showErrorMessage(issue + ` See output window ${outputChannelName} for more information.`);
+      vscode.window.showErrorMessage(
+        issue + ` See output window ${outputChannelName} for more information.`
+      );
       return;
     }
-    if (!toolInitializer.hasInitialized){
+    if (!toolInitializer.hasInitialized) {
       const issue = `Maptz.MCodeCS. Error running command '${commandName}'. The tool has not finished initializing.`;
       outputChannel.appendLine(issue);
-      vscode.window.showErrorMessage(issue + ` See output window ${outputChannelName} for more information.`);
+      vscode.window.showErrorMessage(
+        issue + ` See output window ${outputChannelName} for more information.`
+      );
       return;
     }
-    const configuration = config.Configuration.loadConfiguration(debugMode);
+    const configuration = config.Configuration.loadConfiguration(useDebugMode);
     const td = new tools.ToolInfo(configuration);
 
     const execFile = require("child_process").execFile;
@@ -70,8 +78,8 @@ export class ToolRunner {
           if (!textEditor) {
             rej();
             return;
-          };
-          
+          }
+
           if (err || stderr) {
             vscode.window.showErrorMessage(
               `Error running command. See output window ${outputChannelName} for more information.`
@@ -98,19 +106,18 @@ export class ToolRunner {
             rej(codeChanges.error);
           } else {
             if (!codeChanges.changes || codeChanges.changes.length == 0) return;
-            
-              
-              await textEditor.edit(editBuilder => {
-                for (let i = codeChanges.changes.length - 1; i >= 0; i--) {
+
+            await textEditor.edit(editBuilder => {
+              for (let i = codeChanges.changes.length - 1; i >= 0; i--) {
                 const codeChange = codeChanges.changes[i];
                 const range = new vscode.Range(
                   textEditor.document.positionAt(codeChange.start),
                   textEditor.document.positionAt(codeChange.end)
                 );
                 editBuilder.replace(range, codeChange.newText);
-                }
-              });
-            
+              }
+            });
+
             res();
           }
         }
@@ -138,6 +145,134 @@ export class ToolRunner {
   }
 }
 
+export class ToolRunner2 {
+  constructor() {}
+
+  public async run(commandName: string) {
+    outputChannel.appendLine(`Running command '${commandName}'. Please wait.`);
+    try {
+      await this.runInternal(commandName);
+    } catch (err) {
+      //Swallow the error.
+      console.error(err);
+    }
+    outputChannel.appendLine(`Finished command '${commandName}'.`);
+  }
+
+  private _exe: any;
+
+  public initialize() {
+    if (this._exe) return this._exe;
+
+    const configuration = config.Configuration.loadConfiguration(useDebugMode);
+
+    const td = new tools.ToolInfo(configuration);
+    const execFile = require("child_process").execFile;
+
+    const exeFilePath = td.toolExePath;
+    const args: string[] = [];
+    //Add any exe args. e.g. where you need to run dotnet run -p "" --
+    if (td.toolExeArgs) {
+      const parts = td.toolExeArgs.split(" ");
+      parts.forEach(p => args.push(p));
+    }
+    args.push("stream");
+    const child = execFile(
+      exeFilePath,
+      args,
+      async (err: any, stdout: any, stderr: any) => {
+        if (err || stderr) {
+          vscode.window.showErrorMessage(
+            `Error running command. See output window ${outputChannelName} for more information.`
+          );
+          outputChannel.show();
+          outputChannel.appendLine(`Tool error:`);
+          outputChannel.appendLine(err);
+          outputChannel.appendLine(stderr);
+          return;
+        }
+        outputChannel.appendLine(stdout);
+      }
+    );
+    this._exe = child;
+    return child;
+  }
+
+  private runInternal(commandName: string) {
+    //outputChannel.show();
+
+    const textEditor = vscode.window.activeTextEditor;
+    /* #region  Validate state */
+    if (!textEditor) {
+      outputChannel.appendLine(`No editor. Exiting.`);
+      return;
+    }
+    if (toolInitializer.hasError) {
+      const issue = `Maptz.MCodeCS. Error running command '${commandName}'. The tool has not installed.`;
+      outputChannel.appendLine(issue);
+      vscode.window.showErrorMessage(
+        issue + ` See output window ${outputChannelName} for more information.`
+      );
+      return;
+    }
+    if (!toolInitializer.hasInitialized) {
+      const issue = `Maptz.MCodeCS. Error running command '${commandName}'. The tool has not finished initializing.`;
+      outputChannel.appendLine(issue);
+      vscode.window.showErrorMessage(
+        issue + ` See output window ${outputChannelName} for more information.`
+      );
+      return;
+    }
+    /* #endregion */
+
+    /* #region  Pipe in current document */
+    const cursorOffset = textEditor.document.offsetAt(
+      textEditor.selection.anchor
+    );
+    const fileContents = textEditor.document.getText();
+    const pipedInputModel = {
+      commandName: commandName,
+      cursor: cursorOffset,
+      filePath: textEditor.document.fileName,
+      fileContents: fileContents
+    };
+    const json = JSON.stringify(pipedInputModel);
+
+    var prom = new Promise((res, rej) => {
+      const https = require("https");
+            const options = {
+        hostname: "http://localhost",
+        port: 8089,
+        path: "/",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": json.length
+        }
+      };
+
+      const req = https.request(options, (response: any) => {
+        console.log(`statusCode: ${response.statusCode}`);
+        response.on("data", (d : any) => {
+          process.stdout.write(d);
+          res();
+        });
+      });
+
+      req.on("error", (error: any) => {
+        console.error(error);
+        rej(error);
+      });
+
+      req.write(json);
+      req.end();
+    });
+    return prom;
+
+    /* #endregion */
+  }
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -149,64 +284,61 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
 
+  const toolRunner = new ToolRunner();
+  //const toolRunner = new ToolRunner2(); //Can't get this to work yet!
+  //const exe = toolRunner.initialize();
   const disposables = [];
   disposables.push(
     vscode.commands.registerCommand("mcodecs.addTest", async () => {
-      await new ToolRunner().run("add-test");
+      await toolRunner.run("add-test");
     })
   );
   disposables.push(
     vscode.commands.registerCommand("mcodecs.convertToAsync", async () => {
-      await new ToolRunner().run("convert-to-async");
+      await toolRunner.run("convert-to-async");
     })
   );
   disposables.push(
     vscode.commands.registerCommand(
       "mcodecs.convertToProtectedVirtual",
       async () => {
-        await new ToolRunner().run("convert-to-protected-virtual");
+        await toolRunner.run("convert-to-protected-virtual");
       }
     )
   );
   disposables.push(
     vscode.commands.registerCommand("mcodecs.createSettings", async () => {
-      await new ToolRunner().run("create-settings");
+      await toolRunner.run("create-settings");
     })
   );
   disposables.push(
     vscode.commands.registerCommand("mcodecs.extractClass", async () => {
-      await new ToolRunner().run("extract-class");
+      await toolRunner.run("extract-class");
     })
   );
   disposables.push(
     vscode.commands.registerCommand("mcodecs.expandProperty", async () => {
-      await new ToolRunner().run("expand-property");
+      await toolRunner.run("expand-property");
     })
   );
   disposables.push(
     vscode.commands.registerCommand("mcodecs.expressAsProperty", async () => {
-      await new ToolRunner().run("express-as-property");
+      await toolRunner.run("express-as-property");
     })
   );
   disposables.push(
-    vscode.commands.registerCommand(
-      "mcodecs.expressAsStatement",
-      async () => {
-        await new ToolRunner().run("express-as-statement");
-      }
-    )
+    vscode.commands.registerCommand("mcodecs.expressAsStatement", async () => {
+      await toolRunner.run("express-as-statement");
+    })
   );
   disposables.push(
-    vscode.commands.registerCommand(
-      "mcodecs.removeUnusedUsings",
-      async () => {
-        await new ToolRunner().run("remove-unused-usings");
-      }
-    )
+    vscode.commands.registerCommand("mcodecs.removeUnusedUsings", async () => {
+      await toolRunner.run("remove-unused-usings");
+    })
   );
   disposables.push(
     vscode.commands.registerCommand("mcodecs.sortClass", async () => {
-      await new ToolRunner().run("sort");
+      await toolRunner.run("sort");
     })
   );
 
